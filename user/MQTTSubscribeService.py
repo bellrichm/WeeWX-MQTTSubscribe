8058c17fe1586c96175057fe958e0b56472f1715
Rich Bell <bellrichm@gmail.com>
2019-04-09 14:46:14 -0400

    parse the MQTT payload and update the archive record

--- user/MQTTSubscribeService.py
+++ user/MQTTSubscribeService.py
@@ -1,14 +1,42 @@
+# Proof of concept of a service that subscribes to an MQTT topic 
+# and updates the archive record with the data in the MQTT topic
+#
+# This service consists of two threads. One thread waits on the MQTT
+# topic and when data is recieved it is the queue.
+#
+# The other thread handles the NEW_ARCHIVE_RECORD event. On this
+# event, it processes the queue of MQTT payloads and updates the archive record.
+# This implementation takes last payload in the queue and uses its data to 
+# update the archive record.
+#
+# ToDos (in not particular order)
+# - various ToDos in the code
+# - Additional documentation
+# - Fully support overriding of names in the MQTT payload via the label_map
+# - Handle units and any necessary conversion of the MQTT payload
+# - Handle aggregation of the MQTT payload - or at least ability to customize by overriding
+# - Python 3
+# - Tests
+#
+# Development motivations consist of, but is not limited to, the following:
+# - Get a better understanding of WeeWx internals
+# - Get a better understanding of MQTT
+# - Learn additional Python topics like, threading, Python 3, etc.
+#
+# Version 1.0.0
+
 from __future__ import with_statement
 import syslog
 import Queue
 import paho.mqtt.client as mqtt
 import threading
+import json
 import weewx
 from weewx.engine import StdService
 
 def logmsg(dst, msg):
-    print(msg)
-    syslog.syslog(dst, 'MQTTSUB: %s' % msg)
+    print('MQTTSS: %s' % msg)
+    syslog.syslog(dst, 'MQTTSS: %s' % msg)
 
 def logdbg(msg):
     logmsg(syslog.LOG_DEBUG, msg)
@@ -23,6 +51,8 @@ class MQTTSubscribeService(StdService):
     def __init__(self, engine, config_dict):
         super(MQTTSubscribeService, self).__init__(engine, config_dict)
         
+        self.label_map = {}
+
         service_dict = config_dict.get('MQTTSubscribeService', {})
         host = service_dict.get('host', 'weather-data.local')
         keepalive = service_dict.get('keepalive', 60)
@@ -33,17 +63,54 @@ class MQTTSubscribeService(StdService):
         # ToDo randomize this
         clientid = service_dict.get('clientid', 'MQTTSubscribeService') 
 
-        #log config options
+        #ToDo log config options
         loginf("host is %s" % host)      
         
         self.queue = Queue.Queue() 
         
         self.client = mqtt.Client(client_id=clientid)
+        if username is not None and password is not None:
+            self.client.username_pw_set(username, password)
         
-        self.thread = MQTTSubscribeServiceThread(self, self.queue, self.client, host, keepalive, port, topic, username, password)
+        self.thread = MQTTSubscribeServiceThread(self, self.queue, self.client, host, keepalive, port, topic)
         self.thread.start()
 
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
+
+    def _byteify(self, data, ignore_dicts = False):
+        # if this is a unicode string, return its string representation
+        if isinstance(data, unicode):
+            return data.encode('utf-8')
+        # if this is a list of values, return list of byteified values
+        if isinstance(data, list):
+            return [ self._byteify(item, ignore_dicts=True) for item in data ]
+        # if this is a dictionary, return dictionary of byteified keys and values
+        # but only if we haven't already byteified it
+        if isinstance(data, dict) and not ignore_dicts:
+            data2 = {}
+            for key, value in data.items():
+                key2 = self._byteify(key, ignore_dicts=True)
+                data2[self.label_map.get(key2,key2)] = self._byteify(value, ignore_dicts=True)
+            return data2
+        # if it's anything else, return it in its original form
+        return data
+    
+    # Convert the MQTT payload into a dictionary of archive data usable by WeeWX
+    # In theory, a subclass could override to massage different formatted payloads
+    def create_archive_data(self, json_text):
+        return self._byteify(
+            json.loads(json_text, object_hook=self._byteify),
+            ignore_dicts=True
+    )
+
+    # An overly simplified function to aggregate multiple MQTT payloads into archive data
+    # This, just returns the last payload
+    # This will need major rework to really support aggregation
+    # In the future, this should be easily overridden to support customizing aggregation
+    def aggregate_archive_data(self, aggregate_data, archive_data):
+        # logdbg(archive_data)
+        return archive_data
+
         
     def shutDown(self):
         # when the service shuts down, 
@@ -55,16 +122,21 @@ class MQTTSubscribeService(StdService):
     # put appropriate logic here to process queue and update archive_record
     # queue could have zero or more elements
     def new_archive_record(self, event):
+        aggregate_data = None
         while not self.queue.empty():
             try:
                 msg = self.queue.get_nowait()
-                logdbg(msg.topic)
-                logdbg(msg.payload)
+                # logdbg(msg.topic)
+                # logdbg(msg.payload)
+                archive_data = self.create_archive_data(msg.payload)
+                aggregate_data = self.aggregate_archive_data(aggregate_data, archive_data)
             except Empty, e:
-                break 
+                break
+        # logdbg(aggregate_data)        
+        event.record.update(aggregate_data)
 
-class MQTTSubscribeServiceThread(threading.Thread):
-    def __init__(self, service, queue, client, host, keepalive, port, topic, username, password):
+class MQTTSubscribeServiceThread(threading.Thread): 
+    def __init__(self, service, queue, client, host, keepalive, port, topic):
         threading.Thread.__init__(self)
 
         self.service = service
@@ -74,8 +146,6 @@ class MQTTSubscribeServiceThread(threading.Thread):
         self.keepalive = keepalive
         self.port = port
         self.topic = topic
-        self.username = username
-        self.password = password
         
     def on_message(self, client, userdata, msg):
         #logdbg(msg.topic)
@@ -93,10 +163,7 @@ class MQTTSubscribeServiceThread(threading.Thread):
     def run(self):
         self.client.on_message = self.on_message
         self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
-        
-        if self.username is not None and self.password is not None:
-            self.client.username_pw_set(username, password)
+        self.client.on_disconnect = self.on_disconnect   
       
         self.client.connect(self.host, self.port, self.keepalive)
 
