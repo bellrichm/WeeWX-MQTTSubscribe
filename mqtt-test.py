636fb6384c7ecccfad1622cef9e3da184b2eab43
Rich Bell <bellrichm@gmail.com>
2019-04-23 14:01:48 -0400

    print options

--- mqtt-test.py
+++ mqtt-test.py
@@ -1,21 +1,21 @@
 import paho.mqtt.client as mqtt
 
-MQTT_SERVER = "weather-data.local"
-MQTT_PORT = 1883
-MQTT_KEEPALIVE = 60
-MQTT_TOPIC = 'weather/loop'
-MQTT_CLIENTID = ''
+HOST = "weather-data.local"
+PORT = 1883
+KEEPALIVE = 60
+TOPIC = 'weather/loop'
+CLIENTID = ''
 USERNAME = None
 PASSWORD = None
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe(MQTT_TOPIC)
+    client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
     print('%s: %s\n' %(msg.topic, msg.payload))
 
-client = mqtt.Client(client_id=MQTT_CLIENTID)
+client = mqtt.Client(client_id=CLIENTID)
 
 client.on_connect = on_connect
 client.on_message = on_message
@@ -23,6 +23,17 @@ client.on_message = on_message
 if USERNAME is not None and PASSWORD is not None:
     self.client.username_pw_set(USERNAME, PASSWORD)
 
-client.connect(MQTT_SERVER, MQTT_PORT, MQTT_KEEPALIVE)
+client.connect(HOST, PORT, KEEPALIVE)
 
+print("Host is %s" % HOST)  
+print("Port is %s" % PORT) 
+print("Keep alive is %s" % KEEPALIVE) 
+print("Topic is %s" % TOPIC) 
+print("Client id is %s" % CLIENTID) 
+print("Username is %s" % USERNAME) 
+if PASSWORD is not None:
+    print("Password is set")   
+else:
+    print("Password is not set")
+        
 client.loop_forever()
