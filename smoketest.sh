7a1a975bf90e973cec539b4b67b24b5125ee7e30
Rich Bell <bellrichm@gmail.com>
2019-04-30 09:43:01 -0400

    add some smoke tests

---
+++ smoketest.sh
@@ -0,0 +1,5 @@
+PYTHONPATH=bin python bin/user/MQTTSubscribe.py weewx.conf --type service --binding loop --records 3 --interval 2 --delay 0 --verbose
+PYTHONPATH=bin python bin/user/MQTTSubscribe.py weewx.conf --type driver --binding loop --records 3 --interval 2 --delay 0 --verbose
+
+PYTHONPATH=bin python bin/user/MQTTSubscribe.py weewx.conf --type service --binding archive --records 3 --interval 60 --delay 25 --verbose
+PYTHONPATH=bin python bin/user/MQTTSubscribe.py weewx.conf --type driver --binding archive --records 3 --interval 60 --delay 25 --verbose
