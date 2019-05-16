2965739130ebdc284ca2a8d250bf2feebaaf44d9
Rich Bell <bellrichm@gmail.com>
2019-05-16 10:14:53 -0400

    enable configuring MQTT

--- mqtt-test.py
+++ mqtt-test.py
@@ -1,39 +1,138 @@
-import paho.mqtt.client as mqtt
+#!/usr/bin/python
 
-HOST = "weather-data.local"
-PORT = 1883
-KEEPALIVE = 60
-TOPIC = 'weather/loop'
-CLIENTID = ''
-USERNAME = None
-PASSWORD = None
+import configobj
+import optparse
+import os
+import paho.mqtt.client as mqtt
+import random
+
+usage = """mqtt-test --help
+       mqtt-test [CONFIG_FILE]
+           [--type=[driver|service]]
+           [--host=HOST]
+           [--port=PORT]
+           [--keepalive=KEEPALIVE]
+           [--clientid=CLIENTID]
+           [--username=USERNAME]
+           [--password=PASSWORD]
+           [--topics=TOPIC1,TOPIC2]
+           [--quiet]
+
+
+A simple utility that prints the topis and payloads.
+Configuration can be read from a 'weewx.conf' file or passed in.
+Command line options override any options in the file.
+"""
+
+def on_log(client, userdata, level, msg):
+    log_level = {
+        mqtt.MQTT_LOG_INFO: 'MQTT_LOG_INFO',
+        mqtt.MQTT_LOG_NOTICE: 'MQTT_LOG_NOTICE',
+        mqtt.MQTT_LOG_WARNING: 'MQTT_LOG_WARNING',
+        mqtt.MQTT_LOG_ERR: 'MQTT_LOG_ERR',
+        mqtt.MQTT_LOG_DEBUG: 'MQTT_LOG_DEBUG'
+    }
+
+    print("%s: %s" % (log_level[level], msg))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe(TOPIC)
+    for topic in userdata['topics']:
+        client.subscribe(topic)
 
 def on_message(client, userdata, msg):
     print('%s: %s\n' %(msg.topic, msg.payload))
 
-client = mqtt.Client(client_id=CLIENTID)
+def init_parser():
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("--type", choices=["driver", "service"],
+                    help="The simulation type.",
+                    default="driver")
+    parser.add_option("--host",
+                    help="The MQTT server.")
+    parser.add_option('--port', dest='port', type=int,
+                        help='The port to connect to.')
+    parser.add_option('--keepalive', dest='keepalive', type=int,
+                        help='Maximum period in seconds allowed between communications with the broker.')
+    parser.add_option("--clientid",
+                    help="The clientid to connect with.")
+    parser.add_option("--username",
+                    help="username for broker authentication.")
+    parser.add_option("--password",
+                    help="password for broker authentication.")
+    parser.add_option("--topics",
+                    help="Comma separated list of topics to subscribe to.")
+    parser.add_option("--quiet", action="store_true", dest="quiet",
+                    help="Turn off the MQTT logging.")
 
-client.on_connect = on_connect
-client.on_message = on_message
+    return parser
 
-if USERNAME is not None and PASSWORD is not None:
-    self.client.username_pw_set(USERNAME, PASSWORD)
+def get_option(option, default):
+    if option:
+        return option
+    else:
+        return default
 
-client.connect(HOST, PORT, KEEPALIVE)
+def main():
+    parser = init_parser()
+    (options, args) = parser.parse_args()
 
-print("Host is %s" % HOST)  
-print("Port is %s" % PORT) 
-print("Keep alive is %s" % KEEPALIVE) 
-print("Topic is %s" % TOPIC) 
-print("Client id is %s" % CLIENTID) 
-print("Username is %s" % USERNAME) 
-if PASSWORD is not None:
-    print("Password is set")   
-else:
-    print("Password is not set")
+    if options.type == 'service':
+        config_type = 'MQTTSubscribeService'
+    else:
+        config_type = 'MQTTSubscribeDriver'
+
+    if args:
+        config_path = os.path.abspath(args[0])
+        configuration = configobj.ConfigObj(config_path, file_error=True)
+        config_dict = configuration.get(config_type, {})
+    else:
+        config_dict = {}
         
-client.loop_forever()
+    host = get_option(options.host, config_dict.get('host', 'localhost'))
+    port = get_option(options.port, int(config_dict.get('port', 1883)))
+    keepalive = get_option(options.keepalive, int(config_dict.get('keepalive', 60)))
+    clientid = get_option(options.clientid, config_dict.get('clientid', config_type + '-' + str(random.randint(1000, 9999))))
+    username = get_option(options.username, config_dict.get('username', None))
+    password = get_option(options.password, config_dict.get('password', None))
+
+    if 'topic' in config_dict:
+        topics = config_dict['topic']
+    else:
+        default_topics = []
+        for topic in config_dict['topics']:
+            default_topics.append(topic)
+
+    topics = get_option(options.topics, default_topics)
+
+    print("Host is %s" % host)
+    print("Port is %s" % port)
+    print("Keep alive is %s" % keepalive)
+    print("Client id is %s" % clientid)
+    print("Username is %s" % username)
+    print("Password is %s" % password)
+    print("Topics are %s" % topics)
+
+    if password is not None:
+        print("Password is set")
+    else:
+        print("Password is not set")
+
+    userdata = {}
+    userdata['topics'] = topics
+    client = mqtt.Client(client_id=clientid, userdata=userdata)
+
+    if not options.quiet:
+      client.on_log = on_log
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    if username is not None and password is not None:
+        self.client.username_pw_set(username, password)
+
+    client.connect(host, port, keepalive)
+
+    client.loop_forever()
+
+main()
