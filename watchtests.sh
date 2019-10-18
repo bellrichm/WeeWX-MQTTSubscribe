606e642b92dd934ce5cc51dd8597213c4c96b0c7
Rich Bell <bellrichm@gmail.com>
2019-10-18 16:13:32 -0400

    run both python 2 and 3 tests

--- watchtests.sh
+++ watchtests.sh
@@ -2,5 +2,6 @@
 
 while inotifywait -e modify bin/user/MQTTSubscribe.py bin/user/tests
 do
-PYTHONPATH=/home/pi/weewx/bin python -m unittest discover bin/user/tests
+PYTHONPATH=/home/pi/weewx/bin python2 -m unittest discover bin/user/tests
+PYTHONPATH=/home/pi/weewx/bin python3 -m unittest discover bin/user/tests
 done
