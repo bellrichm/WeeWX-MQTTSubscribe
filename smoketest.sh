68b409df499537a5f2d568248c775b4779349061
Rich Bell <bellrichm@gmail.com>
2019-10-18 20:02:50 -0400

    cleanup development environmet

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,7 @@
 PYTHON=python
 
-PYTHONPATH=bin $PYTHON bin/user/MQTTSubscribe.py weewx.conf --type service --binding loop --records 3 --interval 2 --delay 0 --verbose
-PYTHONPATH=bin $PYTHON bin/user/MQTTSubscribe.py weewx.conf --type driver --binding loop --records 3 --interval 2 --delay 0 --verbose
+PYTHONPATH=bin:../weewx/bin $PYTHON bin/user/MQTTSubscribe.py weewx.loop.conf --type service --binding loop --records 3 --interval 2 --delay 0 --verbose
+PYTHONPATH=bin:../weewx/bin $PYTHON bin/user/MQTTSubscribe.py weewx.loop.conf --type driver --binding loop --records 3 --interval 2 --delay 0 --verbose
 
-PYTHONPATH=bin $PYTHON bin/user/MQTTSubscribe.py weewx.conf --type service --binding archive --records 3 --interval 60 --delay 25 --verbose
-PYTHONPATH=bin $PYTHON bin/user/MQTTSubscribe.py weewx.conf --type driver --binding archive --records 3 --interval 60 --delay 25 --verbose
+PYTHONPATH=bin:../weewx/bin $PYTHON bin/user/MQTTSubscribe.py weewx.archive.conf --type service --binding archive --records 3 --interval 60 --delay 5 --verbose
+PYTHONPATH=bin:../weewx/bin $PYTHON bin/user/MQTTSubscribe.py weewx.archive.conf --type driver --binding archive --records 3 --interval 2 --delay 0 --verbose
--- watchtests.sh
+++ watchtests.sh
@@ -2,6 +2,6 @@
 
 while inotifywait -e modify bin/user/MQTTSubscribe.py bin/user/tests
 do
-PYTHONPATH=/home/pi/weewx/bin python2 -m unittest discover bin/user/tests
-PYTHONPATH=/home/pi/weewx/bin python3 -m unittest discover bin/user/tests
+PYTHONPATH=bin:../weewx/bin python2 -m unittest discover bin/user/tests
+PYTHONPATH=bin:../weewx/bin python3 -m unittest discover bin/user/tests
 done
