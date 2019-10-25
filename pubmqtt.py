078afe9807b2ae1301a7a4f9ef0e9a4b4202a1de
Rich Bell <bellrichm@gmail.com>
2019-10-25 14:59:09 -0400

    scaffolding for #32

---
+++ pubmqtt.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+""" A simple utility that reads messages from a file and publishes each line to MQTT. """
+from __future__ import print_function
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc): # (match callback signature) pylint: disable=unused-argument
+    """ The on_connect callback. """
+    # https://pypi.org/project/paho-mqtt/#on-connect
+    # rc:
+    # 0: Connection successful
+    # 1: Connection refused - incorrect protocol version
+    # 2: Connection refused - invalid client identifier
+    # 3: Connection refused - server unavailable
+    # 4: Connection refused - bad username or password
+    # 5: Connection refused - not authorised
+    # 6-255: Currently unused.
+    print("Connected with result code %i" % rc)
+    print("Connected flags %s" % str(flags))
+
+def on_disconnect(client, userdata, rc): # (match callback signature) pylint: disable=unused-argument
+    """ The on_connect callback. """
+    # https://pypi.org/project/paho-mqtt/#on-connect
+    # rc:
+    # 0: Connection successful
+    print("Disconnected with result code %i" % rc)
+
+def on_publish(client, userdata, mid): # (match callback signature) pylint: disable=unused-argument
+    """ The on_publish callback. """
+    print("Published: %s" %mid)
+
+def main():
+    """ The main entry point. """
+    host = "localhost"
+    port = 1883
+    keepalive = 60
+    client_id = "client_id"
+    topic = "test"
+    message = "testing"
+
+    client = mqtt.Client(client_id)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_publish = on_publish
+    client.connect(host, port, keepalive)
+    client.loop_start()
+    mqtt_message_info = client.publish(topic, message)
+    mqtt_message_info.wait_for_publish()
+    print("Sent: %s has return code %i" %(mqtt_message_info.mid, mqtt_message_info.rc))
+    client.disconnect()
+
+main()
