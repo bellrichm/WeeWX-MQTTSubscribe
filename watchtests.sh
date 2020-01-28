40783b6254e924952398f49a737f0001535fb635
Rich Bell <bellrichm@gmail.com>
2020-01-28 10:09:52 -0500

    watch additional files

--- watchtests.sh
+++ watchtests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-while inotifywait -e modify bin/user/MQTTSubscribe.py bin/user/tests
+while inotifywait -e modify watchtests.sh bin/user/MQTTSubscribe.py bin/user/tests
 do
 PYTHONPATH=bin:../weewx3/bin python2 -m unittest discover bin/user/tests
 PYTHONPATH=bin:../weewx4/bin python2 -m unittest discover bin/user/tests
