7b9e76c43d180a7f3f5ca9ce8e7f232d55d00f4c
Rich Bell <bellrichm@gmail.com>
2020-01-28 17:37:02 -0500

    refactor for code reuse

--- bin/user/tests/func_test.py
+++ bin/user/tests/func_test.py
@@ -22,16 +22,33 @@ class MyThread(threading.Thread):
         pass
 
 class TestJsonPayload(unittest.TestCase):
-    def test_get_data_test(self):
+    def send_msg(self, msg_type, client, topic, topic_info):
+        if msg_type == 'individual':
+            for field in topic_info['data']:
+                mqtt_message_info = client.publish("%s/%s" % (topic, field), topic_info['data'][field])
+                mqtt_message_info.wait_for_publish()
+        elif msg_type == 'json':
+            payload = json.dumps(topic_info['data'])
+            mqtt_message_info = client.publish(topic, payload)
+            mqtt_message_info.wait_for_publish()
+        elif msg_type == 'keyword':
+            msg = ''
+            data = topic_info['data']
+            for field in data:
+                msg = "%s%s%s%s%s" % (msg, field, topic_info['delimiter'], data[field], topic_info['separator'])
+            msg = msg[:-1]
+            msg = msg.encode("utf-8")
+            mqtt_message_info = client.publish(topic, msg)
+            mqtt_message_info.wait_for_publish()
+
+    def get_data_test(self, msg_type):
         sleep = 1
         records = []
 
         config_path = os.path.abspath("bin/user/tests/data/second.conf")
         config_dict = configobj.ConfigObj(config_path, file_error=True)['MQTTSubscribe']
         message_callback_config = config_dict.get('message_callback', None)
-        #message_callback_config['type'] = 'individual'
-        #message_callback_config['type'] = 'json'
-        message_callback_config['type'] = 'keyword'
+        message_callback_config['type'] = msg_type
         #setup_logging(True)
         logger = Logger()
         setup_logging(True)
@@ -54,23 +71,7 @@ class TestJsonPayload(unittest.TestCase):
                     topics = test_data[record][payload]
                     for topic in topics:
                         topic_info = topics[topic]
-                        #individual
-                        #for field in topic_info['data']:
-                            #mqtt_message_info = client.publish("%s/%s" % (topic, field), topic_info['data'][field])
-                            #mqtt_message_info.wait_for_publish()
-                        #json
-                        #payload = json.dumps(topic_info['data'])
-                        #mqtt_message_info = client.publish(topic, payload)
-                        #mqtt_message_info.wait_for_publish()
-                        #keyword
-                        msg = ''
-                        data = topic_info['data']
-                        for field in data:
-                            msg = "%s%s%s%s%s" % (msg, field, topic_info['delimiter'], data[field], topic_info['separator'])
-                        msg = msg[:-1]
-                        msg = msg.encode("utf-8")
-                        mqtt_message_info = client.publish(topic, msg)
-                        mqtt_message_info.wait_for_publish()
+                        self.send_msg(msg_type, client, topic, topic_info)
 
         time.sleep(sleep)
         with open("bin/user/tests/data/second.json") as file_pointer:
@@ -95,6 +96,15 @@ class TestJsonPayload(unittest.TestCase):
         self.assertEqual(records[0]['windDir'], 0)
         self.assertIn('windSpeed', records[0])
         self.assertEqual(records[0]['windSpeed'], 1)
+
+    def test_get_data_individual(self):
+        self.get_data_test('individual')
+
+    def test_get_data_json(self):
+        self.get_data_test('json')
+
+    def test_get_data_keyword(self):
+        self.get_data_test('keyword')
 
 if __name__ == '__main__':
     unittest.main(exit=False)
