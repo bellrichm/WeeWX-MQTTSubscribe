9ed8324adb8b0bd7c563c01fccc675ca5ab350e9
Rich Bell <bellrichm@gmail.com>
2020-01-28 16:15:43 -0500

    convert to unit test format

--- bin/user/tests/func_test.py
+++ bin/user/tests/func_test.py
@@ -1,128 +1,79 @@
-#!/usr/bin/python
-
 # pylint: disable=wrong-import-order
 # pylint: disable=missing-docstring
+
+import unittest
 
 import configobj
 import json
 import os
 import threading
 import time
+
 import paho.mqtt.client as mqtt
 
 from user.MQTTSubscribe import MQTTSubscribe, Logger, setup_logging
 
-EXITFLAG = 0
-
 class MyThread(threading.Thread):
-    def __init__(self, thread_id, name, delay, records):
+    def __init__(self, records):
         threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.name = name
-        self.delay = delay
         self.records = records
 
         config_path = os.path.abspath("bin/user/tests/data/second.conf")
         config_dict = configobj.ConfigObj(config_path, file_error=True)['MQTTSubscribe']
-        #config_dict['host'] = 'weather-data.local'
         message_callback_config = config_dict.get('message_callback', None)
         message_callback_config['type'] = 'json'
         #setup_logging(True)
-        logger = Logger(console=True)
+        logger = Logger()
         setup_logging(True)
         self.subscriber = MQTTSubscribe(config_dict, logger)
         self.subscriber.start()
-        print("test")
 
     def run(self):
-        #print("Starting " + self.name)
-        #print_time(self.name, 5, self.delay)
-        #print("Exiting " + self.name)
-        #records = []
         for data in self.subscriber.get_data('weather/loop'):
             if data:
                 self.records.append(data)
             else:
                 break
-        #print(records)
 
-def print_time(thread_name, counter, delay):
-    while counter:
-        if EXITFLAG:
-            thread_name.exit()
-        time.sleep(delay)
-        print("%s: %s" % (thread_name, time.ctime(time.time())))
-        counter -= 1
+class TestIndividualPayload(unittest.TestCase):
+    def test_get_data_test(self):
+        sleep = 1
+        records = []
+        thread = MyThread(records)
 
-def on_connect(client, userdata, flags, rc):  # (match callback signature) pylint: disable=unused-argument
-    """ The on_connect callback. """
-    # https://pypi.org/project/paho-mqtt/#on-connect
-    # rc:
-    # 0: Connection successful
-    # 1: Connection refused - incorrect protocol version
-    # 2: Connection refused - invalid client identifier
-    # 3: Connection refused - server unavailable
-    # 4: Connection refused - bad username or password
-    # 5: Connection refused - not authorised
-    # 6-255: Currently unused.
-    print("Connected with result code %i" % rc)
-    print("Connected flags %s" % str(flags))
+        client_id = 'clientid'
+        host = 'localhost'
+        port = 1883
+        keepalive = 60
 
+        client = mqtt.Client(client_id)
+        client.connect(host, port, keepalive)
+        client.loop_start()
 
-def on_disconnect(client, userdata, rc):  # (match callback signature) pylint: disable=unused-argument
-    """ The on_connect callback. """
-    # https://pypi.org/project/paho-mqtt/#on-connect
-    # rc:
-    # 0: Connection successful
-    print("Disconnected with result code %i" % rc)
+        with open("bin/user/tests/data/second.json") as fp:
+            test_data = json.load(fp)
+            for record in test_data:
+                for payload in test_data[record]:
+                    topics = test_data[record][payload]
+                    for topic in topics:
+                        topic_info = topics[topic]
+                        payload = json.dumps(topic_info['data'])
+                        mqtt_message_info = client.publish(topic, payload)
+                        mqtt_message_info.wait_for_publish()
 
-def on_publish(client, userdata, mid):  # (match callback signature) pylint: disable=unused-argument
-    """ The on_publish callback. """
-    print("Published: %s" % mid)
+        time.sleep(sleep) # ToDo 
+        thread.start()
+        thread.join()
 
-# Create new threads
-records = []
-THREAD1 = MyThread(1, "Thread-1", 1, records)
-#THREAD2 = MyThread(2, "Thread-2", 2)
+        self.assertEqual(len(records), 1)
+        self.assertIn('dateTime', records[0])
+        self.assertIn('usUnits', records[0])
+        self.assertEqual(records[0]['usUnits'], 1)
 
-# Start new Threads
-#THREAD1.start()
-# sudo /usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
-client_id = 'clientid'
-host = 'localhost'
-port = 1883
-keepalive = 60
+        self.assertIn('windDir', records[0])
+        self.assertEqual(records[0]['windDir'], 0)
+        self.assertIn('windSpeed', records[0])
+        self.assertEqual(records[0]['windSpeed'], 1)
 
-client = mqtt.Client(client_id)
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_publish = on_publish
-client.connect(host, port, keepalive)
-client.loop_start()
-
-topic = 'weather/loop'
-message = 'test'
-#mqtt_message_info = client.publish(topic, message)
-#mqtt_message_info.wait_for_publish()
-#print("Sent: %s has return code %i" % (mqtt_message_info.mid, mqtt_message_info.rc))
-
-with open("bin/user/tests/data/second.json") as fp:
-    test_data = json.load(fp)
-    for record in test_data:
-        for payload in test_data[record]:
-            topics = test_data[record][payload]
-            for topic in topics:
-                topic_info = topics[topic]
-                payload = json.dumps(topic_info['data'])
-                mqtt_message_info = client.publish(topic, payload)
-                mqtt_message_info.wait_for_publish()
-                print("Sent: %s has return code %i" % (mqtt_message_info.mid, mqtt_message_info.rc))
-
-time.sleep(1)
-THREAD1.start()  
-THREAD1.join()
-#THREAD2.start()
-
-print("records")
-print(records)
-print("Exiting Main Thread")
+if __name__ == '__main__':
+    unittest.main(exit=False)
