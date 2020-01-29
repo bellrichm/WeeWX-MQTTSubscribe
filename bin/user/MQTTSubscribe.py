ce898da7fb679dadf8afaac7beabf4a5c31352e9
Rich Bell <bellrichm@gmail.com>
2020-01-29 15:42:37 -0500

    Release 1.5.0 resolves #40, #41, #44

--- bin/user/MQTTSubscribe.py
+++ bin/user/MQTTSubscribe.py
@@ -288,7 +288,7 @@ except ImportError: # pragma: no cover
             if self.console:
                 print('%s: %s' % (__name__, msg))
 
-VERSION = '1.5.0-rc04'
+VERSION = '1.5.0'
 DRIVER_NAME = 'MQTTSubscribeDriver'
 DRIVER_VERSION = VERSION
 
--- install.py
+++ install.py
@@ -28,7 +28,7 @@ if PY2:
 else:
     from io import StringIO
 
-VERSION = '1.5.0-rc04'
+VERSION = '1.5.0'
 
 MQTTSUBSCRIBESERVICE_CONFIG = """
 [MQTTSubscribeService]
