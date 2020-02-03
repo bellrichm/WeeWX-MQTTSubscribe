9b8e2f26789a6941f9116ccdeb05d346f3b1ccb0
Rich Bell <bellrichm@gmail.com>
2020-02-03 14:00:31 -0500

    multiple testruns when testing the service

--- bin/user/tests/func/test_service.py
+++ bin/user/tests/func/test_service.py
@@ -40,12 +40,12 @@ class TestJsonPayload(unittest.TestCase):
             mqtt_message_info = client.publish(topic, msg)
             mqtt_message_info.wait_for_publish()
 
-    def service_test(self, test, config_dict, record):
-        sleep = 3
+    def service_test(self, testtype, testruns, config_dict):
+        sleep = 4
 
         cdict = config_dict['MQTTSubscribeService']
         message_callback_config = cdict.get('message_callback', None)
-        message_callback_config['type'] = test['type']
+        message_callback_config['type'] = testtype
 
         min_config_dict = {
             'Station': {
@@ -74,123 +74,59 @@ class TestJsonPayload(unittest.TestCase):
         client.connect(host, port, keepalive)
         client.loop_start()
 
-        for topics in record:
-            for topic in topics:
-                topic_info = topics[topic]
-                self.send_msg(test['type'], client, topic, topic_info)
+        for testrun in testruns:
+            for topics in testrun['payload']:
+                for topic in topics:
+                    topic_info = topics[topic]
+                    self.send_msg(testtype, client, topic, topic_info)
 
-        time.sleep(sleep)
+            time.sleep(sleep)
 
-        record = {}
-        units = 17
-        interval = 300
-        current_time = int(time.time() + 0.5)
-        end_period_ts = (int(current_time / interval) + 1) * interval
+            record = {}
+            units = testrun['results']['units']
+            interval = 300
+            current_time = int(time.time() + 0.5)
+            end_period_ts = (int(current_time / interval) + 1) * interval
 
-        record['dateTime'] = end_period_ts
-        record['usUnits'] = units
-        new_loop_packet_event = weewx.Event(weewx.NEW_LOOP_PACKET, packet=record)
-        service.new_loop_packet(new_loop_packet_event)
-        #records.append(record)
+            record['dateTime'] = end_period_ts
+            record['usUnits'] = units
+            new_loop_packet_event = weewx.Event(weewx.NEW_LOOP_PACKET, packet=record)
+            service.new_loop_packet(new_loop_packet_event)
+            #records.append(record)
 
+            records = [record]
+            utils.check(self, testtype, records, testrun['results']['records'])
+        
+        #print(records)
         service.shutDown()
         client.disconnect()
 
-        records = [record]
-        utils.check(self, records, test)
-        #print(records)
-
-    def servicex_test(self, test, config_dict, record):
-        sleep = 2
-
-        cdict = config_dict['MQTTSubscribeService']
-        message_callback_config = cdict.get('message_callback', None)
-        message_callback_config['type'] = test['type']
-
-        min_config_dict = {
-            'Station': {
-                'altitude': [0, 'foot'],
-                'latitude': 0,
-                'station_type': 'Simulator',
-                'longitude': 0
-            },
-            'Simulator': {
-                'driver': 'weewx.drivers.simulator',
-            },
-            'Engine': {
-                'Services': {}
-            }
-        }
-
-        engine = StdEngine(min_config_dict)
-
-        service = MQTTSubscribeService(engine, config_dict)
-
-        host = 'localhost'
-        port = 1883
-        keepalive = 60
-
-        client = mqtt.Client()
-        client.connect(host, port, keepalive)
-        client.loop_start()
-
-        records = []
-        for topics in record:
-            for topic in topics:
-                topic_info = topics[topic]
-                self.send_msg(test['type'], client, topic, topic_info)
-
-                time.sleep(sleep)
-
-                record = {}
-                units = 17
-                interval = 300
-                current_time = int(time.time() + 0.5)
-                end_period_ts = (int(current_time / interval) + 1) * interval
-
-                record['dateTime'] = end_period_ts
-                record['usUnits'] = units
-                new_loop_packet_event = weewx.Event(weewx.NEW_LOOP_PACKET, packet=record)
-                service.new_loop_packet(new_loop_packet_event)
-                records.append(record)
-
-        service.shutDown()
-        client.disconnect()
-
-        #records = [record]
-        utils.check(self, records, test)
-        #print(records)
-
-# do i want to accumulate packets into records? or have multiple records?
-
-    #unittest.skip("")
+    #@unittest.skip("")
     def test_get_data_individual1(self):
         with open("bin/user/tests/func/data/first.json") as file_pointer:
             testx_data = json.load(file_pointer, object_hook=utils.byteify)
             config_dict = configobj.ConfigObj(testx_data['config'])
-            test_data = testx_data['data']['payload']
-            for test in testx_data['data']['results']:
-                # print(test)
-                self.service_test(test, config_dict, test_data)
+            # TODO Skip
+            for testtype in testx_data['types']:
+                self.service_test(testtype, testx_data['data'], config_dict)
 
-    #unittest.skip("")
+    #@unittest.skip("")
     def test_get_data_individual2(self):
         with open("bin/user/tests/func/data/firsty.json") as file_pointer:
             testx_data = json.load(file_pointer, object_hook=utils.byteify)
             config_dict = configobj.ConfigObj(testx_data['config'])
-            test_data = testx_data['data']['payload']
-            for test in testx_data['data']['results']:
-                # print(test)
-                self.service_test(test, config_dict, test_data)
+            # TODO Skip
+            for testtype in testx_data['types']:
+                self.service_test(testtype, testx_data['data'], config_dict)
 
+    #@unittest.skip("")
     def test_get_data_individual3(self):
-        with open("bin/user/tests/func/data/accumulatedrain.json") as file_pointer:
+        with open("bin/user/tests/func/data/accumulatedraina.json") as file_pointer:
             testx_data = json.load(file_pointer, object_hook=utils.byteify)
             config_dict = configobj.ConfigObj(testx_data['config'])
-            test_data = testx_data['data']['payload']
-            for test in testx_data['data']['results']:
-                # print(test)
-                self.servicex_test(test, config_dict, test_data)
+            # TODO Skip
+            for testtype in testx_data['types']:
+                self.service_test(testtype, testx_data['data'], config_dict)
 
     #def test_get_data_individual(self):
         #self.driver_test('individual')
