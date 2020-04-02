ac4768dc92bc9c4d51a36bae1d0d60f7e02f2b25
Rich Bell <bellrichm@gmail.com>
2020-04-02 16:10:40 -0400

    ongoing cleanup

--- appveyor/install.sh
+++ appveyor/install.sh
@@ -5,3 +5,15 @@
     unzip -qq -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
     export PATH=$SONAR_SCANNER_HOME/bin:$PATH
     export SONAR_SCANNER_OPTS="-server"
+
+    echo "Running mosquitto install"
+    sudo apt-get -qq --assume-yes install mosquitto
+
+    echo "Running pip installs"
+    pip install configobj --quiet --no-python-version-warning
+    pip install paho-mqtt --quiet --no-python-version-warning
+    pip install mock --quiet --no-python-version-warning
+    pip install pylint --quiet --no-python-version-warning
+    pip install coveralls --quiet --no-python-version-warning
+    pip install nose --quiet --no-python-version-warning
+    pip install coverage --quiet --no-python-version-warning
