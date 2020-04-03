3da839e4f16cc135386509e1963d9020ae12f8f0
Rich Bell <bellrichm@gmail.com>
2020-04-02 20:00:06 -0400

    var debug

--- appveyor/install.sh
+++ appveyor/install.sh
@@ -1,5 +1,5 @@
-    export SONAR_SCANNER_VERSION=4.2.0.1873
-    export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+    #export SONAR_SCANNER_VERSION=4.2.0.1873
+    #export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
     echo "Running sonar runner install"
     curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
     unzip -qq -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
