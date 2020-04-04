1fab022ab17a3f36265775929a9fe260131f73cf
Rich Bell <bellrichm@gmail.com>
2020-04-04 12:58:19 -0400

    coveralls test

--- appveyor/init.sh
+++ appveyor/init.sh
@@ -4,4 +4,7 @@ export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 if [ "$APPVEYOR_REPO_TAG" = "true" ]; then
   export APPVEYOR_BUILD_VERSION=release-$APPVEYOR_REPO_TAG_NAME
   appveyor UpdateBuild -Version "release-$APPVEYOR_REPO_TAG_NAME"
+
+  export CI_BRANCH=master
+  export CI_BUILD_NUMBER=$APPVEYOR_BUILD_VERSION
 fi
