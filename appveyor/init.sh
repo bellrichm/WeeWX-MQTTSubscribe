374c9f5802dbcf9eb398543af85a43380b34edb8
Rich Bell <bellrichm@gmail.com>
2020-04-04 08:07:37 -0400

    fix up overriding version

--- appveyor/init.sh
+++ appveyor/init.sh
@@ -1,10 +1,7 @@
 export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 
-echo $APPVEYOR_REPO_TAG
-echo $APPVEYOR_REPO_TAG_NAME
-
 if [ "$APPVEYOR_REPO_TAG" = "true" ]; then
-  echo APPVEYOR_BUILD_VERSION=$APPVEYOR_REPO_TAG
-  appveyor UpdateBuild -Version "$APPVEYOR_REPO_TAG"
+  export APPVEYOR_BUILD_VERSION=$APPVEYOR_REPO_TAG
+  appveyor UpdateBuild -Version "$APPVEYOR_REPO_TAG_NAME"
 fi
