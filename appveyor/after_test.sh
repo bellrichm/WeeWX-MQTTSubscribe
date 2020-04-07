17691e99133c56c1838b66dcb7727ed4814bbbf5
Rich Bell <bellrichm@gmail.com>
2020-04-07 15:11:40 -0400

    Codecov (#51)
    * unit vs integration tests
    
    * quiet codecov
    
    * verbose
    
    * name test
    
    * upload naming
    
    * Codecov.io badge
    
    * merge coveralls
    
    * bash experiment
    
    * give up merging coveralls
    would need to hack a step into appveyor after all buils complete

--- appveyor/after_test.sh
+++ appveyor/after_test.sh
@@ -2,7 +2,8 @@
 
 # only upload once
 if [ "$CODECOVIO_UPLOAD" = "true" ]; then
-  bash <(curl -s https://codecov.io/bash)
+  bash <(curl -s https://codecov.io/bash) -f coverage.xml -F unitests -n unitests-$PYTHON #>/dev/null
+  bash <(curl -s https://codecov.io/bash) -f coverage2.xml -F integration -n integration-$PYTHON #/dev/null
 fi
 
 # only upload once
