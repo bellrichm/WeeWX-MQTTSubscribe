1cb13e577f96e82e634b1008fb767fb1576eb7f8
Rich Bell <bellrichm@gmail.com>
2020-04-08 14:47:52 -0400

    read the docs...

--- appveyor/init.sh
+++ appveyor/init.sh
@@ -2,7 +2,7 @@ export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linu
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 
 if [ "$APPVEYOR_REPO_TAG" = "true" ]; then
-  export APPVEYOR_BRANCH_NAME=$APPVEYOR_REPO_TAG_NAME # hack
+  export APPVEYOR_REPO_BRANCH=$APPVEYOR_REPO_TAG_NAME # hack
   export APPVEYOR_BUILD_VERSION=release-$APPVEYOR_REPO_TAG_NAME
   appveyor UpdateBuild -Version "release-$APPVEYOR_REPO_TAG_NAME"
 fi
