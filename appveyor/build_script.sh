109ede02f7c36b9dee4cec20fd9d5c680157e38d
Rich Bell <bellrichm@gmail.com>
2020-04-16 15:08:57 -0400

    message example

--- appveyor/build_script.sh
+++ appveyor/build_script.sh
@@ -3,3 +3,6 @@ if [ "$ENABLED" != "true" ]; then
 fi
 
 PYTHONPATH=./weewx/bin pylint ./bin/user/MQTTSubscribe.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.txt
+rc = $?
+
+appveyor AddMessage "test $rc" -Details `cat pylint.txt`
