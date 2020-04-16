67622c9650af0b4edfd602bc72205dcd86f7aff7
Rich Bell <bellrichm@gmail.com>
2020-04-16 15:33:50 -0400

    better messaging

--- appveyor/build_script.sh
+++ appveyor/build_script.sh
@@ -3,7 +3,7 @@ if [ "$ENABLED" != "true" ]; then
 fi
 
 PYTHONPATH=./weewx/bin pylint ./bin/user/MQTTSubscribe.py -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.txt
-rc=$?
+rc=${PIPESTATUS[0]}
 detail=`cat pylint.txt`
 
-appveyor AddMessage "test $rc" -Details "$detail"
+appveyor AddMessage "pylint $WEEWX $PYTHON: $rc" -Details "$detail"
