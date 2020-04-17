9b546e3e447dcba87f3f4a57ae3f433f04e43538
Rich Bell <bellrichm@gmail.com>
2020-04-17 07:48:23 -0400

    up version indicator

--- bin/user/MQTTSubscribe.py
+++ bin/user/MQTTSubscribe.py
@@ -284,7 +284,7 @@ except ImportError: # pragma: no cover
             if self.console:
                 print('%s: %s' % (__name__, msg))
 
-VERSION = '1.5.3-rc02'
+VERSION = '1.5.3-rc03'
 DRIVER_NAME = 'MQTTSubscribeDriver'
 DRIVER_VERSION = VERSION
 
--- install.py
+++ install.py
@@ -28,7 +28,7 @@ if PY2:
 else:
     from io import StringIO
 
-VERSION = '1.5.3-rc02'
+VERSION = '1.5.3-rc03'
 
 MQTTSUBSCRIBESERVICE_CONFIG = """
 [MQTTSubscribeService]
