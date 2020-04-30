3d375ae3c1501ce0171560c4bd3b876319d2fad4
Rich Bell <bellrichm@gmail.com>
2020-04-30 10:19:07 -0400

    driver tests for handling wind

--- bin/user/tests/integ/test_get_accumulated_data.py
+++ bin/user/tests/integ/test_get_accumulated_data.py
@@ -124,7 +124,7 @@ class TestOne(unittest.TestCase):
             self.runit(config_dict, testruns, payload)
 
     #@unittest.skip("")
-    def test_wind_individual_opic(self):
+    def test_wind_individual_topic(self):
         payload = 'individual'
         with open("bin/user/tests/integ/data/wind-individual.json") as file_pointer:
             test_data = json.load(file_pointer, object_hook=utils.byteify)
