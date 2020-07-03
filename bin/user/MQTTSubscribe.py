292520bc904558beb58cc44d6d611cb7232c9a8d
Rich Bell <bellrichm@gmail.com>
2020-07-03 12:02:05 -0400

    up version indicator

--- bin/user/MQTTSubscribe.py
+++ bin/user/MQTTSubscribe.py
@@ -311,7 +311,7 @@ from weewx.engine import StdService
 import weeutil
 from weeutil.weeutil import option_as_list, to_bool, to_float, to_int, to_sorted_string
 
-VERSION = '1.6.0'
+VERSION = '1.6.1-rc01'
 DRIVER_NAME = 'MQTTSubscribeDriver'
 DRIVER_VERSION = VERSION
 
