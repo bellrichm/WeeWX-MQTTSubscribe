c814a60bc6a6be0ed202cf2fc16abe44d0b99b91
Rich Bell <bellrichm@gmail.com>
2020-07-05 13:59:51 -0400

    simple tests forva simple example

---
+++ bin/user/tests/unit/test_ExampleCallbackProvider.py
@@ -0,0 +1,78 @@
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-docstring
+# pylint: disable=invalid-name
+# pylint: disable=protected-access
+
+import unittest
+import mock
+
+import random
+import string
+import xml.etree
+
+from user.ExampleMessageCallbackProvider import MessageCallbackProvider
+
+class Msg(object):
+    # pylint: disable=too-few-public-methods
+    def __init__(self, topic, payload, qos, retain):
+        self.topic = topic
+        self.payload = payload
+        self.qos = qos
+        self.retain = retain
+
+class Test1(unittest.TestCase):
+    @staticmethod
+    def create_observation_dict(size):
+        observation_dict = {}
+
+        i = 0
+        while i < size:
+            # pylint: disable=unused-variable
+            observation_dict[''.join([random.choice(string.ascii_letters) for n in range(32)])] = round(random.uniform(101, 200), 2)
+            i += 1
+
+        return observation_dict
+
+    @staticmethod
+    def create_observation_xml(parent, observation_dict):
+        xml_element = xml.etree.ElementTree.Element(parent)
+        for key, value in observation_dict.items():
+            child = xml.etree.ElementTree.Element(key)
+            child.text = str(value)
+            xml_element.append(child)
+        return xml_element
+
+
+    def test_get_observations(self):
+        observation_dict = self.create_observation_dict(5)
+        root = self.create_observation_xml('observations', observation_dict)
+
+        SUT = MessageCallbackProvider({}, None, None)
+
+        results = SUT.get_observations(root, "", {}, 1)
+
+        self.assertDictEqual(observation_dict, results)
+
+    def test_on_message(self):
+        mock_logger = mock.Mock()
+        mock_manager = mock.Mock()
+        mock_manager.get_fields.return_value = {}
+
+        observation_dict = self.create_observation_dict(5)
+        root = self.create_observation_xml('observations', observation_dict)
+        xml_string = xml.etree.ElementTree.tostring(root)
+        msg = Msg("topic", xml_string, 0, 0)
+
+        SUT = MessageCallbackProvider({}, mock_logger, mock_manager)
+
+        SUT._on_message(None, None, msg)
+
+        mock_manager.append_data.assert_called_once_with(msg.topic, observation_dict)
+
+if __name__ == '__main__':
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(Test1('test_get_observations'))
+    test_suite.addTest(Test1('test_on_message'))
+    unittest.TextTestRunner().run(test_suite)
+
+    #unittest.main(exit=False)
