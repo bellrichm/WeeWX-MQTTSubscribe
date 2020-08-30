83b044dbceeacedcb59f0135914b3d77bf30a0a0
Rich Bell <bellrichm@gmail.com>
2020-08-30 11:04:54 -0400

    unit tests are not dependenrbon WeeWX

--- runutests.sh
+++ runutests.sh
@@ -3,20 +3,20 @@
 export PYENV_VERSION=2.7.17
 export WEEWX=weewx4
 echo "Running python $PYENV_VERSION $WEEWX"
-PYTHONPATH=bin:../$WEEWX/bin python -m unittest discover bin/user/tests/unit
+PYTHONPATH=bin python -m unittest discover bin/user/tests/unit
 
 export PYENV_VERSION=3.6.9
 export WEEWX=weewx4
 echo "Running python $PYENV_VERSION $WEEWX"
-PYTHONPATH=bin:../$WEEWX/bin python -m unittest discover bin/user/tests/unit
+PYTHONPATH=bin python -m unittest discover bin/user/tests/unit
 
 export PYENV_VERSION=3.5.9
 export WEEWX=weewx4
 echo "Running python $PYENV_VERSION $WEEWX"
-PYTHONPATH=bin:../$WEEWX/bin python -m unittest discover bin/user/tests/unit
+PYTHONPATH=bin python -m unittest discover bin/user/tests/unit
 
 PYENV_VERSION=2.7.17
 export WEEWX=weewx3
 echo "Running python $PYENV_VERSION $WEEWX"
-PYTHONPATH=bin:../$WEEWX/bin python -m unittest discover bin/user/tests/unit
+PYTHONPATH=bin python -m unittest discover bin/user/tests/unit
 
