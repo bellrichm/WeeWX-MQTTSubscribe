2abf04308ba4bdee1d67a2bf3571fd7a1ee0d50a
Rich Bell <bellrichm@gmail.com>
2020-08-30 11:05:10 -0400

    tests completed

--- bin/user/tests/unit/test_AbstractMessageCallbackProvider.py
+++ bin/user/tests/unit/test_AbstractMessageCallbackProvider.py
@@ -1,5 +1,6 @@
 # pylint: disable=wrong-import-order
 # pylint: disable=missing-docstring
+# pylint: disable=invalid-name
 
 from __future__ import with_statement
 
@@ -9,20 +10,74 @@ import mock
 import random
 import string
 
+import test_weewx_stubs # needed to import AbstractMessageCallbackProvider pylint: disable=unused-import
+
 from user.MQTTSubscribe import AbstractMessageCallbackProvider, Logger, TopicManager
 
 class TestTest(unittest.TestCase):
-    def test_test(self):
-        print("start")
+    def test_contains_total_invalid_previous_value(self):
         mock_logger = mock.Mock(spec=Logger)
         mock_manager = mock.Mock(spec=TopicManager)
 
-        with mock.patch('weewx.units') as mock_weewx_units:
+        with mock.patch('weewx.units'):
             SUT = AbstractMessageCallbackProvider(mock_logger, mock_manager)
 
-            mock_weewx_units.getStandardUnitType.return_value = ('foo', 'bar')
-            converted_value = round(random.uniform(10, 100), 2)
-            mock_weewx_units.convert.return_value = (converted_value, 'foo', 'bar')
+            orig_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
+            previous_value = round(random.uniform(101, 200), 2)
+
+            SUT.previous_values = {
+                orig_name: previous_value
+            }
+
+            new_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
+
+            fields = {
+                orig_name: {
+                    'contains_total': True
+                }
+            }
+
+            orig_value = round(random.uniform(10, 100), 2)
+            unit_system = random.randint(1, 99)
+
+            (updated_name, updated_value) = SUT._update_data(fields, orig_name, orig_value, unit_system) # pylint: disable=protected-access
+
+            self.assertEqual(updated_name, orig_name)
+            self.assertIsNone(updated_value)
+            self.assertEqual(SUT.previous_values[orig_name], orig_value)
+
+    def test_contains_total_no_previous_value(self):
+        mock_logger = mock.Mock(spec=Logger)
+        mock_manager = mock.Mock(spec=TopicManager)
+
+        with mock.patch('weewx.units'):
+            SUT = AbstractMessageCallbackProvider(mock_logger, mock_manager)
+
+            orig_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
+
+            new_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
+
+            fields = {
+                orig_name: {
+                    'contains_total': True
+                }
+            }
+
+            orig_value = round(random.uniform(10, 100), 2)
+            unit_system = random.randint(1, 99)
+
+            (updated_name, updated_value) = SUT._update_data(fields, orig_name, orig_value, unit_system) # pylint: disable=protected-access
+
+            self.assertEqual(updated_name, orig_name)
+            self.assertIsNone(updated_value)
+            self.assertEqual(SUT.previous_values[orig_name], orig_value)
+
+    def test_contains_total(self):
+        mock_logger = mock.Mock(spec=Logger)
+        mock_manager = mock.Mock(spec=TopicManager)
+
+        with mock.patch('weewx.units'):
+            SUT = AbstractMessageCallbackProvider(mock_logger, mock_manager)
 
             orig_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
             previous_value = round(random.uniform(0, 9), 2)
@@ -35,9 +90,39 @@ class TestTest(unittest.TestCase):
 
             fields = {
                 orig_name: {
+                    'contains_total': True
+                }
+            }
+
+            orig_value = round(random.uniform(10, 100), 2)
+            unit_system = random.randint(1, 99)
+
+            (updated_name, updated_value) = SUT._update_data(fields, orig_name, orig_value, unit_system) # pylint: disable=protected-access
+
+            self.assertEqual(updated_name, orig_name)
+            self.assertEqual(updated_value, orig_value - previous_value)
+            self.assertEqual(SUT.previous_values[orig_name], orig_value)
+
+    def test_converting_value(self):
+        mock_logger = mock.Mock(spec=Logger)
+        mock_manager = mock.Mock(spec=TopicManager)
+
+        with mock.patch('weewx.units') as mock_weewx_units:
+            SUT = AbstractMessageCallbackProvider(mock_logger, mock_manager)
+
+            mock_weewx_units.getStandardUnitType.return_value = \
+                (''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]),
+                 ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
+            converted_value = round(random.uniform(10, 100), 2)
+            mock_weewx_units.convert.return_value = (converted_value,
+                                                     ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]),
+                                                     ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
+
+            orig_name = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])  # pylint: disable=unused-variable
+
+            fields = {
+                orig_name: {
                     'units': ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]),  # pylint: disable=unused-variable
-                    'contains_total': True,
-                    'name': new_name
                 }
             }
 
@@ -46,18 +131,9 @@ class TestTest(unittest.TestCase):
 
             (updated_name, updated_value) = SUT._update_data(fields, orig_name, orig_value, unit_system) # pylint: disable=protected-access
 
-            print("%s %f" % (updated_name, updated_value))
-            print("%s %f" % (orig_name, orig_value))
-            print("%f" % (converted_value))
-            self.assertEqual(updated_name, new_name)
-            self.assertEqual(updated_value, converted_value - previous_value)
-            self.assertEqual(SUT.previous_values[orig_name], converted_value)
-
-            print("done")
+            self.assertEqual(updated_name, orig_name)
+            self.assertEqual(updated_value, converted_value)
+            self.assertEqual(SUT.previous_values, {})
 
 if __name__ == '__main__':
-    # test_suite = unittest.TestSuite()
-    # test_suite.addTest(TestInitialization('test_connect_exception'))
-    # unittest.TextTestRunner().run(test_suite)
-
     unittest.main(exit=False)
