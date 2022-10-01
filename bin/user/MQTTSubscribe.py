3ad2044c316b8c239c367f6996096b8fb6da0f81
Rich Bell <bellrichm@gmail.com>
2022-10-01 06:56:24 -0400

    dev prep

--- bin/user/MQTTSubscribe.py
+++ bin/user/MQTTSubscribe.py
@@ -400,7 +400,7 @@ import weewx
 import weewx.drivers
 from weewx.engine import StdEngine, StdService
 
-VERSION = '2.2.0'
+VERSION = '2.2.1-rc01a'
 DRIVER_NAME = 'MQTTSubscribeDriver'
 DRIVER_VERSION = VERSION
 
--- install.py
+++ install.py
@@ -31,7 +31,7 @@ if PY2:
 else:
     from io import StringIO
 
-VERSION = '2.2.0'
+VERSION = '2.2.1-rc01a'
 
 MQTTSUBSCRIBESERVICE_CONFIG = """
 [MQTTSubscribeService]
