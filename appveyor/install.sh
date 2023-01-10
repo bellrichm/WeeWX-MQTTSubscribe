6aa37358ae755c18a508fd8e377a166ff72f6384
Rich Bell <bellrichm@gmail.com>
2023-01-10 12:59:02 -0500

    testing

--- appveyor/install.sh
+++ appveyor/install.sh
@@ -27,7 +27,7 @@
     pip install coveralls --quiet --no-python-version-warning
 
     echo "Running weewx install"
-    echo "$BRANCH"
+    echo "$BRANCH $WEEWX"
     if [ "$WEEWX" = "master" ]; then
       git clone https://github.com/weewx/weewx.git weewx
       cd weewx
