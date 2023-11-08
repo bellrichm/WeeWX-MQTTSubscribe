da1487348a7eb04519856abd9b55432daf62029a
Rich Bell <bellrichm@gmail.com>
2023-11-08 10:44:34 -0500

    use pytest

--- devtools/coverage.sh
+++ devtools/coverage.sh
@@ -7,10 +7,10 @@
 source ./devtools/python_versions.sh
 
 export PYENV_VERSION=$weewx3_default_python_version
-PYTHONPATH=bin coverage run -p --branch -m unittest discover bin/user/tests/unit; 
+PYTHONPATH=bin coverage run -p --branch -m pytest bin/user/tests/unit; 
 
 export PYENV_VERSION=$weewx4_default_python_version
-PYTHONPATH=bin coverage3 run -p --branch -m unittest discover bin/user/tests/unit; 
+PYTHONPATH=bin coverage3 run -p --branch -m pytest bin/user/tests/unit; 
 
 coverage combine
 coverage html --include bin/user/MQTTSubscribe.py
