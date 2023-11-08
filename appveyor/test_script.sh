b3553a18beb88709f7768d74c4387e7cfefdd566
Rich Bell <bellrichm@gmail.com>
2023-11-08 11:11:25 -0500

    upload test results after every run

--- appveyor/test_script.sh
+++ appveyor/test_script.sh
@@ -23,8 +23,13 @@ fi
 if [ "$BUILDTYPE" = "LOCAL" ]; then
 	HTML_OPTIONS=" --cov-report html:cover "
 fi
- PYTHONPATH=bin pytest ./bin/user/tests/unit --junitxml=results.xml --cov-report xml:coverage.xml --verbosity=1 --log-level=ERROR --cov=user.MQTTSubscribe --cov-branch $HTML_OPTIONS
- rc=$?
+
+PYTHONPATH=bin pytest ./bin/user/tests/unit --junitxml=results.xml --cov-report xml:coverage.xml --verbosity=1 --log-level=ERROR --cov=user.MQTTSubscribe --cov-branch $HTML_OPTIONS
+rc=$?
+
+if [ "$BUILDTYPE" != "LOCAL" ]; then
+  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
+fi
 
 # coveralls uses this file, so stash a copy
 mv .coverage .coverage1
@@ -37,6 +42,10 @@ fi
 
 PYTHONPATH=bin:$PPATH pytest ./bin/user/tests/func --junitxml=results3.xml --verbosity=1 --log-level=ERROR
 rc=$?
+
+if [ "$BUILDTYPE" != "LOCAL" ]; then
+  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results3.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
+fi
 
 # ToDo - option to not exit on error - for debugging
 if [ $rc -ne 0 ]; then
@@ -51,12 +60,11 @@ fi
  PYTHONPATH=bin:$PPATH pytest ./bin/user/tests/integ --junitxml=results2.xml --cov-report xml:coverage2.xml --verbosity=1 --log-level=ERROR --cov=user.MQTTSubscribe --cov-branch $HTML_OPTIONS
  rc=$?
 
+if [ "$BUILDTYPE" != "LOCAL" ]; then
+  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results2.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
+fi
+
  # coveralls uses this file, so stash a copy
 cp .coverage .coverage2
 
-if [ "$BUILDTYPE" != "LOCAL" ]; then
-  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
-  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results3.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
-  find "$APPVEYOR_BUILD_FOLDER" -type f -name 'results2.xml' -print0 | xargs -0 -I '{}' curl -F 'file=@{}' "https://ci.appveyor.com/api/testresults/junit/$APPVEYOR_JOB_ID"
-fi
 exit $rc
