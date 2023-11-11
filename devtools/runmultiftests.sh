acda7663442921d391a805c52405e47b480eb6f2
Rich Bell <bellrichm@gmail.com>
2023-11-11 09:25:22 -0500

    run muli func tests

---
+++ devtools/runmultiftests.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+#    Copyright (c) 2023 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+source ./devtools/python_versions.sh
+
+for version in ${weewx4_python_versions[@]}; do
+    ./devtools/runftests.sh weewx4 $version
+done
+
+for version in ${weewx5_python_versions[@]}; do
+    ./devtools/runftests.sh weewx5 $version
+done
