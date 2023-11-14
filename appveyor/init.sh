a00f1f2311d93ea9c610e3c90fa653b5a83b7a79
Rich Bell <bellrichm@gmail.com>
2023-11-14 09:17:10 -0500

    attempt 2 to specify jdk sonar requires newer version

--- appveyor/init.sh
+++ appveyor/init.sh
@@ -1,10 +1,13 @@
 #
-#    Copyright (c) 2020-2021 Rich Bell <bellrichm@gmail.com>
+#    Copyright (c) 2020-2023 Rich Bell <bellrichm@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
 export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$JAVA_HOME:$PATH
 
 if [ "$APPVEYOR_REPO_TAG" = "true" ]; then
   export APPVEYOR_REPO_BRANCH=$APPVEYOR_REPO_TAG_NAME # hack
