6feaad3daa6f2aeaf3df0f0a245b28a9ac3faf3d
Rich Bell <bellrichm@gmail.com>
2023-12-20 08:11:46 -0500

    pragma location needs to be on line to ignore

--- bin/user/MQTTSubscribe.py
+++ bin/user/MQTTSubscribe.py
@@ -2977,8 +2977,7 @@ For more information see, https://github.com/bellrichm/WeeWX-MQTTSubscribe/wiki/
 
         weecfg.save(self.config_dict, self.config_output_path, not self.no_backup)
 
-    def _update(self):
-        # tested as part of the integration tests # pragma no cover
+    def _update(self): # tested as part of the integration tests # pragma no cover
         if self.action == '--add-from':
             weeutil.config.conditional_merge(self.config_dict[self.section], self.config_input_dict)
         elif self.action == '--remove':
@@ -3006,8 +3005,7 @@ For more information see, https://github.com/bellrichm/WeeWX-MQTTSubscribe/wiki/
         for msg in error_msgs:
             print(msg)
 
-    def _update_interactively(self):
-        # pragma: no cover
+    def _update_interactively(self): # pragma: no cover
         conf_editor = MQTTSubscribeDriverConfEditor()
         conf_editor.existing_options = self.config_dict.get(self.section, {})
         settings = conf_editor.prompt_for_settings()
