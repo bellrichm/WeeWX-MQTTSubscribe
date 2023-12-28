fabc702351b6f335f469e80b77f102139bb93038
Rich Bell <bellrichm@gmail.com>
2023-12-28 12:33:28 -0500

    test simulate driver

--- bin/user/tests/unit/test_MQTTSubscribeSimulator.py
+++ bin/user/tests/unit/test_MQTTSubscribeSimulator.py
@@ -53,8 +53,6 @@ class TestRunSimulator(unittest.TestCase):
 
                 mock_engine = mock.Mock()
                 SUT.engine = mock_engine
-                #SUT.config_dict = {}
-                #SUT.config_dict['MQTTSubscribeDriver'] = {}
 
                 SUT.simulate_service_archive()
 
@@ -92,13 +90,74 @@ class TestRunSimulator(unittest.TestCase):
 
                 mock_engine = mock.Mock()
                 SUT.engine = mock_engine
-                #SUT.config_dict = {}
-                #SUT.config_dict['MQTTSubscribeDriver'] = {}
 
                 SUT.simulate_service_packet()
 
         mock_time.sleep.assert_called_once_with(sleep_amount)
         mock_engine.dispatchEvent.assert_called_once()
+
+    def test_simulate_driver_archive(self):
+        data = {}
+        data['dateTime'] = 0
+
+        archive_delay = 5
+        archive_interval = 60
+        now = time.time()
+
+        current_time = int(now + 0.5)
+        end_period_ts = (int(current_time / archive_interval) + 1) * archive_interval
+        end_delay_ts = end_period_ts + archive_delay
+        sleep_amount = end_delay_ts - current_time
+
+        options = argparse.Namespace()
+        options.type = 'driver'
+        options.binding = 'archive'
+        options.record_count = len(data)
+        options.console = None
+        options.conf = None
+        options.verbose = None
+        options.log_file = None
+
+        options.archive_delay = archive_delay
+        options.archive_interval = archive_interval
+
+        SUT = Simulator(None, options)
+
+        with mock.patch('user.MQTTSubscribe.time') as mock_time:
+            mock_time.time.return_value = now
+
+            mock_driver = mock.Mock()
+            mock_driver.genArchiveRecords.return_value = [data]
+
+            SUT.simulate_driver_archive(mock_driver)
+
+        mock_time.sleep.assert_called_once_with(sleep_amount)
+        mock_driver.genArchiveRecords.assert_called_once_with(end_period_ts)
+
+    def test_simulate_driver_loop(self):
+        data = {}
+        data['dateTime'] = 0
+
+        options = argparse.Namespace()
+        options.type = 'driver'
+        options.binding = 'loop'
+        options.record_count = len(data)
+        options.console = None
+        options.conf = None
+        options.verbose = None
+        options.log_file = None
+
+        options.archive_delay = None
+        options.archive_interval = None
+
+        SUT = Simulator(None, options)
+
+        mock_driver = mock.Mock()
+        mock_driver.genLoopPackets.return_value = [data]
+
+        SUT.simulate_driver_packet(mock_driver)
+
+        mock_driver.genLoopPackets.assert_called_once()
 
 if __name__ == '__main__':
     # test_suite = unittest.TestSuite()
