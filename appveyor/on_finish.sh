abee4237bed149e951c89c70d69f06bb5e8c00ea
Rich Bell <bellrichm@gmail.com>
2024-03-12 15:51:24 -0400

    fine tune options to stop build and ssh into

--- appveyor/on_finish.sh
+++ appveyor/on_finish.sh
@@ -3,7 +3,7 @@
 #
 #    See the file LICENSE.txt for your full rights.
 #
- if [ "$ENABLE_SSH_AT_END" = "true" ]; then
+ if [ "$ENABLE_SSH_AT_END" == "true" ] && [ "$STOP_WITH_BUILD_IMAGE" = "$APPVEYOR_BUILD_WORKER_IMAGE" ] && [ "$STOP_WITH_PYTHON_VERSION" = "$PYTHON" ] ; then
     export APPVEYOR_SSH_BLOCK=true
     curl -sflL 'https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-ssh.sh' | bash -e -
 fi
