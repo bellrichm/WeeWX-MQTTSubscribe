a6c2c059848950fb5301f91cd45a50c80f2ee878
Rich Bell <bellrichm@gmail.com>
2024-03-12 13:53:37 -0400

    fine tune ssh support

--- appveyor/on_finish.sh
+++ appveyor/on_finish.sh
@@ -1,9 +1,9 @@
 #
-#    Copyright (c) 2020-2021 Rich Bell <bellrichm@gmail.com>
+#    Copyright (c) 2020-2024 Rich Bell <bellrichm@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
- if [ "$ENABLE_SSH" = "true" ]; then
+ if [ "$ENABLE_SSH_AT_END" = "true" ]; then
     export APPVEYOR_SSH_BLOCK=true
     curl -sflL 'https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-ssh.sh' | bash -e -
 fi
