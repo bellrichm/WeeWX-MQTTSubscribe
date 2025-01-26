db968530a6e545bca8993d2a1b54f2646faa6020
Rich Bell <bellrichm@gmail.com>
2025-01-26 17:04:08 -0500

    save the engine

--- bin/user/tests/e2e/simulator.py
+++ bin/user/tests/e2e/simulator.py
@@ -11,14 +11,16 @@ DRIVER_NAME = 'Simulator'
 
 # See, for more information on what could be done.
 
-def loader(config_dict, _engine):
+def loader(config_dict, engine):
     '''Load and return the driver. '''
     start_ts, resume_ts = weewx.drivers.simulator.extract_starts(config_dict, DRIVER_NAME)
-    station = Simulator(start_time=start_ts, resume_time=resume_ts, **config_dict[DRIVER_NAME])
+    station = Simulator(engine, start_time=start_ts, resume_time=resume_ts, **config_dict[DRIVER_NAME])
     return station
 
 class Simulator(weewx.drivers.simulator.Simulator):
     ''' A simulator driver to be used when testing WeeWX/MQTTSubscribe.'''
     # (methods not used) pylint: disable=abstract-method
-    def __init__(self, **stn_dict):
+    def __init__(self, engine, **stn_dict):
         super().__init__(**stn_dict)
+
+        self.engine = engine
